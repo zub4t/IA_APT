57b7305cb7d61ce89798d6ea041228b78d2c91c9
Marco Augusto <30879430+zub4t@users.noreply.github.com>
2020-04-30 16:43:54 +0100

    teste

--- src/main/java/com/mycompany/gerador/Gerador.java
+++ src/main/java/com/mycompany/gerador/Gerador.java
@@ -17,13 +17,14 @@ public class Gerador {
 
     static List<Ret> list;
     static Map<Ponto, List<Ret>> map = new TreeMap<>();
+    static Map<Integer, Ponto> dicionario = new TreeMap<>();
 
     public static void main(String[] args) {
         list = new ArrayList<Ret>();
         try {
             File myObj = new File("C:/Users/marco/Documents/NetBeansProjects/Gerador/src/main/java/com/mycompany/gerador/input.txt");
             Scanner myReader = new Scanner(myObj);
-            myReader.nextLine();
+            int num_ret = myReader.nextInt();
             while (myReader.hasNext()) {
                 int id = Integer.parseInt(myReader.next());
                 int n_points = Integer.parseInt(myReader.next());
@@ -57,16 +58,21 @@ public class Gerador {
                 }
 
             }
+            int id_pontos = 1;
+            int arr[] = new int[13];
             for (Entry<Ponto, List<Ret>> entry : map.entrySet()) {
                 Ponto key = entry.getKey();
                 List<Ret> value = entry.getValue();
-
+                arr[id_pontos] = value.size();
+                dicionario.put(id_pontos, key);
+                id_pontos++;
                 System.out.printf("%s\n", key);
                 for (Ret model : value) {
                     System.out.println(model);
                 }
 
             }
+            greedy(arr, (num_ret + 1));
             myReader.close();
         } catch (Exception e) {
             System.out.println("An error occurred." + e);
@@ -74,6 +80,33 @@ public class Gerador {
         }
     }
 
+    public static void greedy(int arr[], int n) {
+        Heapmax heap = new Heapmax(arr, n);
+        heap.write_heap();
+        int id_ponto = heap.extractMax();
+        decrease(id_ponto, heap);
+    }
+
+    public static void decrease(int id_ponto, Heapmax heap) {
+        Ponto p = dicionario.get(id_ponto);
+        List<Ret> list = map.get(p);
+        for (Ret ret : list) {
+            for (Entry<Integer, Ponto> entry : dicionario.entrySet()) {
+                int ponto = entry.getKey();
+                List<Ret> list_aux = map.get(dicionario.get(ponto));
+                for (int i = 0; i < list_aux.size(); i++) {
+                    if (list_aux.get(i) == ret) {
+                        i--;
+                        list_aux.remove(ret);
+                        heap.increaseKey(ponto, list_aux.size());
+                    }
+
+                }
+                map.put(dicionario.get(ponto), list_aux);
+
+            }
+        }
+    }
 }
 
 class Ret implements Comparable<Ret> {
